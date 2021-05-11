cdd5706de75e825331695fca52dc8faeeef7c475
silvertw <silver-computacion@hotmail.com>
2021-05-11 01:17:57 -0300

    crear alimentos

--- router/alimentos.js
+++ router/alimentos.js
@@ -24,4 +24,20 @@ router.get('/', async(req, res) => {
     
 })
 
+
+router.get('/crear', (req, res)=>{
+    res.render('crear');
+})
+
+
+router.post('/', async (req, res)=>{
+    const body=req.body;
+    try {
+        const alimentoDB= new Alimento(body);
+        await alimentoDB.save();
+        res.redirect("/alimentos")
+    } catch (error) {
+        console.log(error);
+    }
+})
 module.exports = router;
